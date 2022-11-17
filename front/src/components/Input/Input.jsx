dc80dafe952fe174a3022490e22ef592c5601ceb
Klark-Kalahan <carrillosolisivan2001@gmail.com>
2022-11-16 23:34:36 -0500

    New Components

---
+++ front/src/components/Input/Input.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+const Input = (props) => {
+    const {store, USUA_usuario, setUSUA_usuario, USUA_Password, setUSUA_Password} = props;
+
+    return (
+        <input type="text" name="email" id="email"
+               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+               placeholder="name@company.com"
+               value={USUA_usuario}
+              onChange={ (e)=> setUSUA_usuario(e.target.value)}
+               required=""/>
+    );
+};
+
+/*Button.defaultProps = {
+    type: 'primary',
+    children: null,
+    className: '',
+    inline: false
+};*/
+
+export default Input ;
--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Button from "../../components/Button/Button.jsx";
+import Input from "../../components/Input/Input.jsx";
 const Login = (props) => {
     const {store, USUA_usuario, setUSUA_usuario, USUA_Password, setUSUA_Password} = props;
     return (
@@ -7,7 +9,6 @@ const Login = (props) => {
                 <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                     <img className="mr-2" style={{ height: '9rem' , width: '12rem' }} src="http://www.dubaumetalindustria.com/dubau/assets/media/logos/logo-light.png?1"
                          alt="logo"/>
-
                 </a>
                 <div
                     className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -20,12 +21,7 @@ const Login = (props) => {
                                 <label htmlFor="email"
                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                                     username</label>
-                                <input type="text" name="email" id="email"
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       placeholder="name@company.com"
-                                       value={USUA_usuario}
-                                       onChange={ (e)=> setUSUA_usuario(e.target.value)}
-                                       required=""/>
+                                <Input  USUA_usuario={USUA_usuario} setUSUA_usuario={setUSUA_usuario}  />
                             </div>
                             <div>
                                 <label htmlFor="password"
@@ -41,11 +37,7 @@ const Login = (props) => {
                                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
                                     password?</a>
                             </div>
-                            <button type="submit"
-                                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                                Sign
-                                in
-                            </button>
+                            <Button />
                         </form>
                     </div>
                 </div>
